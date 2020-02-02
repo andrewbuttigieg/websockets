92922a091ac39e3a9480eeaaf3ef6f18f53c344f
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2020-02-02 12:13:51 +0100

    Adds dotnet websocket producer

---
+++ producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer (8080, false);
+            wssv.AddWebSocketService<Laputa> ("/Laputa");
+            wssv.Start ();
+            Console.ReadKey (true);
+            wssv.Stop ();
+        }
+    }
+
+    public class Laputa : WebSocketBehavior
+  {
+    protected override void OnMessage (MessageEventArgs e)
+    {
+      var msg = e.Data == "BALUS"
+                ? "I've been balused already..."
+                : "I'm not available now.";
+
+      Send (msg);
+    }
+  }
+}
